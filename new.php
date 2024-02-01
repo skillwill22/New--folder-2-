ca571ab7ab41e556431e23bafc730fb9a67c3518
unknown <g.pirveli22@.gmail.com>
2024-02-01 13:57:30 +0400

    first

---
+++ new.php
@@ -0,0 +1,16 @@
+<?php
+
+$name= 'Giorgi';
+ 
+$password= "Pirveli"
+
+$confirmation=$password
+
+
+
+if ($name && $confirmation=$password) {
+    echo "თქვენ წარმატებით დარეგისტრირდით";
+} else {
+    echo "პაროლი და მისი
+    კონფირმაცია არასწორია";
+}
